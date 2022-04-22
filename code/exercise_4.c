e87bea8159ef81b649d84f6359bbc0efef31fc62
Sidorets Kirill <sidorets.kirill97@gmail.com>
2022-04-22 13:52:20 +0200

    factorize code

--- code/exercise_4.c
+++ code/exercise_4.c
@@ -111,7 +111,18 @@ void print_buffer(double* buffer, int n)
 	printf("\n");
 }
 
-
+int rank_Calc(lbm_comm_t * comm,int cord0,int cord1){
+	int coords[2];
+	int rank;
+	coords[0] = comm->rank_x+cord0;
+	coords[1] = comm->rank_y+cord1;
+	if (coords[0] < 0 || coords[1] < 0 || coords[0] >= comm->nb_x || coords[1] >= comm->nb_y) {
+		rank = MPI_PROC_NULL;
+	} else {
+		MPI_Cart_rank(comm->communicator, coords, &rank);
+	}
+	return rank;
+}
 /****************************************************/
 void lbm_comm_ghost_exchange_ex4(lbm_comm_t * comm, lbm_mesh_t * mesh)
 {
@@ -126,88 +137,24 @@ void lbm_comm_ghost_exchange_ex4(lbm_comm_t * comm, lbm_mesh_t * mesh)
 	int rank_dl;
 	int rank_ur;
 	int rank_ul;
-	int coords_r[2];
-	int coords_l[2];
-	int coords_u[2];
-	int coords_d[2];
-	int coords_dr[2];
-	int coords_dl[2];
-	int coords_ur[2];
-	int coords_ul[2];
 
 	// RIGHT communication.
-	coords_r[0] = comm->rank_x+1;
-	coords_r[1] = comm->rank_y;
-	if (coords_r[0] < 0 || coords_r[1] < 0 || coords_r[0] >= comm->nb_x || coords_r[1] >= comm->nb_y) {
-		rank_r = MPI_PROC_NULL;
-	} else {
-		MPI_Cart_rank(comm->communicator, coords_r, &rank_r);
-	}
-
+	rank_r =rank_Calc(comm,1,0); 
 	// LEFT communication.
-	coords_l[0] = comm->rank_x-1;
-	coords_l[1] = comm->rank_y;
-	if (coords_l[0] < 0 || coords_l[1] < 0 || coords_l[0] >= comm->nb_x || coords_l[1] >= comm->nb_y) {
-		rank_l = MPI_PROC_NULL;
-	} else {
-		MPI_Cart_rank(comm->communicator, coords_l, &rank_l);
-	}
-
+	rank_l =rank_Calc(comm,-1,0);
 	// UP communication.
-	coords_u[0] = comm->rank_x;
-	coords_u[1] = comm->rank_y-1;
-	if (coords_u[0] < 0 || coords_u[1] < 0 || coords_u[0] >= comm->nb_x || coords_u[1] >= comm->nb_y) {
-		rank_u = MPI_PROC_NULL;
-	} else {
-		MPI_Cart_rank(comm->communicator, coords_u, &rank_u);
-	}
-
+	rank_u =rank_Calc(comm,0,-1);
 	// DOWN communication.
-	coords_d[0] = comm->rank_x;
-	coords_d[1] = comm->rank_y+1;
-	if (coords_d[0] < 0 || coords_d[1] < 0 || coords_d[0] >= comm->nb_x || coords_d[1] >= comm->nb_y) {
-		rank_d = MPI_PROC_NULL;
-	} else {
-		MPI_Cart_rank(comm->communicator, coords_d, &rank_d);
-	}
-
+	rank_d =rank_Calc(comm,0,1);
 	// DOWN-RIGHT communication.
-	coords_dr[0] = comm->rank_x+1;
-	coords_dr[1] = comm->rank_y+1;
-	if (coords_dr[0] < 0 || coords_dr[1] < 0 || coords_dr[0] >= comm->nb_x || coords_dr[1] >= comm->nb_y) {
-		rank_dr = MPI_PROC_NULL;
-	} else {
-		MPI_Cart_rank(comm->communicator, coords_dr, &rank_dr);
-	}
-
+	rank_dr =rank_Calc(comm,1,1);
 	// DOWN-LEFT communication.
-	coords_dl[0] = comm->rank_x-1;
-	coords_dl[1] = comm->rank_y+1;
-	if (coords_dl[0] < 0 || coords_dl[1] < 0 || coords_dl[0] >= comm->nb_x || coords_dl[1] >= comm->nb_y) {
-		rank_dl = MPI_PROC_NULL;
-	} else {
-		MPI_Cart_rank(comm->communicator, coords_dl, &rank_dl);
-	}
-
-	// TODO: UP-RIGHT communication.
-	coords_ur[0] = comm->rank_x+1;
-	coords_ur[1] = comm->rank_y-1;
-	if (coords_ur[0] < 0 || coords_ur[1] < 0 || coords_ur[0] >= comm->nb_x || coords_ur[1] >= comm->nb_y) {
-		rank_ur = MPI_PROC_NULL;
-	} else {
-		MPI_Cart_rank(comm->communicator, coords_ur, &rank_ur);
-	}
-
+	rank_dl =rank_Calc(comm,-1,1);
+	// UP-RIGHT communication.
+	rank_ur =rank_Calc(comm,1,-1);
 	// UP-LEFT communication.
-	coords_ul[0] = comm->rank_x-1;
-	coords_ul[1] = comm->rank_y-1;
-	if (coords_ul[0] < 0 || coords_ul[1] < 0 || coords_ul[0] >= comm->nb_x || coords_ul[1] >= comm->nb_y) {
-		printf("Coordinates [%d, %d], I'm Process %d,%d\n", coords_ul[0], coords_ul[1], comm->rank_x, comm->rank_y);
-		rank_ul = MPI_PROC_NULL;
-	} else {
-		MPI_Cart_rank(comm->communicator, coords_ul, &rank_ul);
-	}
-
+	rank_ul =rank_Calc(comm,-1,-1);
+	
 	// Receive from L, send to R, receive from R, send to L.
 	// Receive from U, send to D, receive from D, send to U.
 	if (rank_l != MPI_PROC_NULL)
