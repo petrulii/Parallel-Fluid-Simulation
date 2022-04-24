f85d1b7b39b1c41eb8dc081498fb6babc5d8e7c9
Sidorets Kirill <sidorets.kirill97@gmail.com>
2022-04-24 18:56:46 +0200

    Ex 5 , 6 and python mesurment script

--- code/measurement.py
+++ code/measurement.py
@@ -3,16 +3,38 @@ import os
 import matplotlib.pyplot as plt
 X = [2,4,8]
 plt.figure()
-for i in range(1,7):
+for i in range(1,4):
         Y=[]
         for j in X:
                 cmd = "mpirun --use-hwthread-cpus -np "+str(j)+" ./lbm -e "+str(i)+" -n"
-                stream = os.popen(cmd)
-                output = stream.read()
-                splited = output.split("\n")
-                time = splited[-2].split(" ")[2]
-                Y.append(float(time))
+                sumT=0
+                for h in range(0,10):
+                        print(i,j,h)
+                        stream = os.popen(cmd)
+                        output = stream.read()
+                        splited = output.split("\n")
+                        time = splited[-2].split(" ")[2]
+                        sumT=sumT+float(time)
+                Y.append((sumT/10))
         plt.plot(X, Y, '.-' ,  label="Ex "+str(i))
 plt.legend(loc="upper left")
-plt.savefig('measurements')
+plt.savefig('measurements123')
+plt.show()
+plt.figure()
+for i in range(4,7):
+        Y=[]
+        for j in X:
+                cmd = "mpirun --use-hwthread-cpus -np "+str(j)+" ./lbm -e "+str(i)+" -n"
+                sumT=0
+                for h in range(0,10):
+                        print(i,j,h)
+                        stream = os.popen(cmd)
+                        output = stream.read()
+                        splited = output.split("\n")
+                        time = splited[-2].split(" ")[2]
+                        sumT=sumT+float(time)
+                Y.append((sumT/10))
+        plt.plot(X, Y, '.-' ,  label="Ex "+str(i))
+plt.legend(loc="upper left")
+plt.savefig('measurements456')
 plt.show()
